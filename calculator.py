57c01031bcae735e168ff37e5ccec040976c5314
puka0 <113772329+puka0@users.noreply.github.com>
2022-10-01 11:08:44 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+
+print("Возможные операции: +, -, *, /, #")
+oper = input("Введите операцию")
+if oper != '#':
+    number1 = int(input('Введите первое число'))
+    number2 = int(input('Введите второе число'))
+if oper == '+':
+    print(number1 + number2)
+elif oper == '-':
+    print(number1 - number2)
+elif oper == '*':
+    print(number1 * number2)
+elif oper == '/':
+    print(number1 / number2)
+else:
+    a = int(input('Введите a'))
+    b = int(input('Введите b'))
+    c = int(input('Введите c'))
+    D = b * b - 4*a*c
+    x1 = (-b + D ** (1 / 2)) / (2 * a)
+    print("x1=" + str(x1))
+    x2 = (-b - D ** (1 / 2)) / (2 * a)
+    print("x2=" + str(x2))
