40e68743d182435860adb6c47f8254f7bc4be20a
DanyaYanev <113249729+DanyaYanev@users.noreply.github.com>
2022-10-06 12:58:41 +0300

    Update 15_чисел БЕЗ WHILE.py

--- 15_чисел БЕЗ WHILE.py
+++ 15_чисел БЕЗ WHILE.py
@@ -1,9 +1,9 @@
 sum_count = 0        # счетчик суммы отриц. чисел
-multiply_count = 1   # счетчик произведения положит. чисел
+multiply_count = 0   # счетчик произведения положит. чисел
 for i in range(15):
     x = int(input('Введите число: '))
     if x > 0:
-        multiply_count = multiply_count * x
+        multiply_count = multiply_count + x
     elif x < 0:
         sum_count = sum_count + x
     else:
@@ -11,5 +11,6 @@ for i in range(15):
 
 
 print('Сумма отрицательных чисел = ' + str(sum_count))
-print('Произведение положительных чисел = ' + str(multiply_count))
+print('Суммаположительных чисел = ' + str(multiply_count))
+
 
